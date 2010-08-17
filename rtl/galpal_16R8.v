8bcf942b0755083153ffa3ac1e58cfdf2b69dce5
Jason S. McMullan <jason.mcmullan@gmail.com>
2010-08-17 00:18:11 -0400

    [16R8] PAL16R8 placeholder
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

---
+++ rtl/galpal_16R8.v
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010, Jason S. McMullan. All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+module galpal_16R8 (
+	input	CLK,		// Pin 1
+	input	[8:1] I,	// Pin 2-9
+	input	_OE,		// Pin 11
+	output	[8:1] O,	// Pin 12-19
+	input	VCC,		// Pin 20
+	input	GND		// Pin 10
+);
+
+/* TODO! */
+
+endmodule
